988160d456cfa7052b7ee3b875c7050b0e93c0ab
Nyctef <nyctef@nyctef.com>
2025-02-01 11:03:47 +0000

    initial commit: copying code from rgcompare experimental branch

---
+++ avalonia-test/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace avalonia_test.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ avalonia-test/Views/LiveDatasource.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace avalonia_test.Views;
+
+public partial class LiveDatasource : UserControl
+{
+    public LiveDatasource()
+    {
+        InitializeComponent();
+    }
+}
